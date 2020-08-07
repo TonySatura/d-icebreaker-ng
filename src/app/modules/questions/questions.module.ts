83c4e01dab6d8fd502b272653688f20db9ceccf8
TonySatura <tony.satura@awin.com>
2020-08-07 13:43:58 +0200

    Moves cache functionality to separate module.

--- src/app/modules/questions/questions.module.ts
+++ src/app/modules/questions/questions.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { SharedModule } from "../shared/shared.module";
-import { CacheService } from "../shared/services/cache.service";
-import { CacheSettings, CacheStorageType } from "../shared/models/cache.model";
-import { CACHE_SETTINGS } from "../shared/services/cache.config";
+import { CacheSettings, CacheStorageType } from "../cache/models/cache.model";
+import { CacheService } from "../cache/services/cache.service";
+import { CACHE_SETTINGS } from "../cache/services/cache.config";
 
 const QUESTIONS_CACHE_SETTINGS: CacheSettings = {
   enabled: true,
-  expiresInSeconds: 10 * 60,
+  expiresInSeconds: 24 * 60 * 60,
   storageType: CacheStorageType.IN_MEMORY,
 };
 
 @NgModule({
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule],
   providers: [
     CacheService,
     {
--- src/app/modules/questions/services/question.service.ts
+++ src/app/modules/questions/services/question.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-import { CacheService } from "../../shared/services/cache.service";
+import { CacheService } from "../../cache/services/cache.service";
 
 @Injectable({
   providedIn: "root",
