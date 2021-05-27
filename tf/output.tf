95a497ca39221e681d13b8f9f1dc06a65f1a328d
TonySatura <tony.satura@awin.com>
2021-05-27 16:18:36 +0200

    Shows output variables for terraform.

--- tf/output.tf
+++ tf/output.tf
@@ -7,3 +7,8 @@ output "website_url" {
   value       = "https://${aws_cloudfront_distribution.ui_distribution.domain_name}"
   description = "The public exposed URL of the website application."
 }
+
+output "github_connection_hint" {
+  value       = "The resource ${aws_codestarconnections_connection.github_connection.name} was initially created in the state PENDING. Authentication with the connection provider must be completed in the AWS Console."
+  description = "The public exposed URL of the website application."
+}
